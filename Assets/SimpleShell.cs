f385214df7c41979fdf52680187634f46eaec01c
Garrett Gunnell <gunnellg@oregonstate.edu>
2023-10-02 19:17:29 -0700

    add empty script for a basic shell

---
+++ Assets/SimpleShell.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleShell : MonoBehaviour {
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
