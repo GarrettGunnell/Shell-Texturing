d1fbcdeea117b739a96e46f5059decd2ff5bffcd
Garrett Gunnell <gunnellg@oregonstate.edu>
2023-10-12 04:43:50 -0700

    Implement FXAA

--- Assets/FXAA.cs
+++ Assets/FXAA.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class FXAA : MonoBehaviour {
     public Shader fxaaShader;
 
+    [Range(0.0312f, 0.0833f)]
+    public float contrastThreshold = 0.0312f;
+
+    [Range(0.063f, 0.333f)]
+    public float relativeThreshold = 0.063f;
+
+    [Range(0.0f, 1.0f)]
+    public float subpixelBlending = 1.0f;
+
     private Material fxaaMaterial;
 
     void OnEnable() {
@@ -13,7 +22,18 @@ public class FXAA : MonoBehaviour {
     }
 
     void OnRenderImage(RenderTexture source, RenderTexture destination) {
-        Graphics.Blit(source, destination, fxaaMaterial);
+        var luminanceTex = RenderTexture.GetTemporary(source.width, source.height, 0, RenderTextureFormat.RHalf);
+        
+        fxaaMaterial.SetFloat("_ContrastThreshold", contrastThreshold);
+        fxaaMaterial.SetFloat("_RelativeThreshold", relativeThreshold);
+        fxaaMaterial.SetFloat("_SubpixelBlending", subpixelBlending);
+
+        Graphics.Blit(source, luminanceTex, fxaaMaterial, 0);
+
+        fxaaMaterial.SetTexture("_LuminanceTex", luminanceTex);
+
+        Graphics.Blit(source, destination, fxaaMaterial, 1);
+        RenderTexture.ReleaseTemporary(luminanceTex);
     }
 
     void OnDisable() {
