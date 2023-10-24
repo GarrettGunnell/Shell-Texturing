b4b2fb60c3b0022bbc217c08215c0808ce0dd4ca
Garrett Gunnell <gunnellg@oregonstate.edu>
2023-10-23 19:41:56 -0700

    Annotate cpu side code

--- Assets/SimpleShell.cs
+++ Assets/SimpleShell.cs
@@ -9,6 +9,8 @@ public class SimpleShell : MonoBehaviour {
 
     public bool updateStatics = true;
 
+    // These variables and what they do are explained on the shader code side of things
+    // You can see below which shader uniforms match up with these variables
     [Range(1, 256)]
     public int shellCount = 16;
 
@@ -58,13 +60,13 @@ public class SimpleShell : MonoBehaviour {
             shells[i] = new GameObject("Shell 1");
             shells[i].AddComponent<MeshFilter>();
             shells[i].AddComponent<MeshRenderer>();
-
-            Vector3 rotation = new Vector3(0, 0, 0);
-            shells[i].transform.eulerAngles = rotation;
             
             shells[i].GetComponent<MeshFilter>().mesh = shellMesh;
             shells[i].GetComponent<MeshRenderer>().material = shellMaterial;
             shells[i].transform.parent = this.transform;
+
+            // In order to tell the GPU what its uniform variable values should be, we use these "Set" functions which will set the
+            // values over on the GPU. 
             shells[i].GetComponent<MeshRenderer>().material.SetInt("_ShellCount", shellCount);
             shells[i].GetComponent<MeshRenderer>().material.SetInt("_ShellIndex", i);
             shells[i].GetComponent<MeshRenderer>().material.SetFloat("_ShellLength", shellLength);
@@ -87,24 +89,36 @@ public class SimpleShell : MonoBehaviour {
         Vector3 direction = new Vector3(0, 0, 0);
         Vector3 oppositeDirection = new Vector3(0, 0, 0);
 
+        // This determines the direction we are moving from wasd input. It's probably a better idea to use Unity's input system, since it handles
+        // all possible input devices at once, but I did it the old fashioned way for simplicity.
         direction.x = Convert.ToInt32(Input.GetKey(KeyCode.D)) - Convert.ToInt32(Input.GetKey(KeyCode.A));
         direction.y = Convert.ToInt32(Input.GetKey(KeyCode.W)) - Convert.ToInt32(Input.GetKey(KeyCode.S));
         direction.z = Convert.ToInt32(Input.GetKey(KeyCode.Q)) - Convert.ToInt32(Input.GetKey(KeyCode.E));
 
+        // This moves the ball according the input direction
         Vector3 currentPosition = this.transform.position;
         direction.Normalize();
         currentPosition += direction * velocity * Time.deltaTime;
         this.transform.position = currentPosition;
 
+        // This changes the direction that the hair is going to point in, when we are not inputting any movements then we subtract the gravity vector
+        // The gravity vector just being (0, -1, 0)
         displacementDirection -= direction * Time.deltaTime * 10.0f;
         if (direction == Vector3.zero)
             displacementDirection.y -= 10.0f * Time.deltaTime;
 
         if (displacementDirection.magnitude > 1) displacementDirection.Normalize();
 
-
+        // In order to avoid setting this variable on every single shell's material instance, we instead set this is as a global shader variable
+        // That every shader will have access to, which sounds bad, because it kind of is, but just be aware of your global variable names and it's not a big deal.
+        // Regardless, setting the variable one time instead of 256 times is just better.
         Shader.SetGlobalVector("_ShellDirection", displacementDirection);
 
+        // Generally it is bad practice to update statics that do not need to be updated every frame
+        // You can see the performance difference between updating 256 shells of statics by disabling the updateStatics parameter in the script
+        // So it obviously matters at the extreme ends, but something above like setting the directional vector each frame is not going to make an insane diff
+        // You will see in my other shaders and scripts that I do not always do this, because I'm lazy, but it's best practice to not update what doesn't need to be
+        // updated.
         if (updateStatics) {
             for (int i = 0; i < shellCount; ++i) {
                 shells[i].GetComponent<MeshRenderer>().material.SetInt("_ShellCount", shellCount);
